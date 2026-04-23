ee57be9697451210612c767f65fc7b7f0a6a7504
Dmitry Sergeev <dmitrijsergeev111@gmail.com>
2026-04-23 13:28:10 +0300

    Add main logic

--- src/logic.h
+++ src/logic.h
@@ -1,5 +1,32 @@
 #include <stdio.h>
-void readGraph();
+#include <stdlib.h>
 
-void findBestCity();
+typedef struct Node {
+    int target;
+    int weight;
+    struct Node* next;
+} Node;
 
+typedef struct {
+    int cityIndex;
+    int dist; 
+} HeapElement;
+
+typedef struct {
+    HeapElement* data;
+    int size;
+    int capacity;
+} MinHeap;
+
+Node** roadsRead(char* fileName,int* capitalCount, int* cities, int*roads, FILE** capitals);
+
+void addEdge(Node** graph, int city1, int city2, int roadLen);
+
+void printStatesAndCities(Node** graph, int capitalCount, int cities, FILE* capitals);
+void push(MinHeap* heap, HeapElement el);
+
+void siftUp(MinHeap* heap, int idx);
+
+HeapElement pop(MinHeap* heap);
+void siftDown(MinHeap* heap, int idx);
+void initHeap(MinHeap* heap, int initCapacity);
